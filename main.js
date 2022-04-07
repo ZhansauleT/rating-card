5e2c622cc3d2a2d6cb9ad42b076263576e72d2c0
Zhansaule Telisheva <jansaule97@mail.ru>
2022-04-07 13:46:54 +0500

    rating card design is finished

---
+++ main.js
@@ -0,0 +1,45 @@
+const ratingNumbers = document.querySelectorAll(".card-rating-numbers > ul > li");
+const submitButton = document.querySelector(".submit");
+const answer = document.querySelector(".answer");
+
+let ratingValue =  ratingNumbers.innerHTML;
+let pressed = false;
+
+if(window.location.href === "http://127.0.0.1:5500/index.html") {
+
+
+
+ratingNumbers.forEach((item) => {
+  item.addEventListener('click', (e) => {
+
+    e.target.classList.toggle("pressed");
+    
+    if(e.target.classList.contains("pressed")){
+      pressed = true;
+      ratingValue = e.target.innerHTML;
+      localStorage.setItem("rating", JSON.stringify(ratingValue));
+    }else {
+      pressed = false;
+    }
+
+    //e.target.classList.remove("pressed");
+    
+  });
+});
+
+submitButton.addEventListener("click", () => {
+
+  if(pressed){
+    location.href = "thankyou.html";
+  }
+
+});
+
+}
+
+if (window.location.href === "http://127.0.0.1:5500/thankyou.html"){
+
+  let result = JSON.parse(localStorage.getItem('rating'));
+  answer.innerHTML = +result;
+}
+
